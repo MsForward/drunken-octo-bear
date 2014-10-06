87c059317d7576beaff447edfd8501f4374c3f57
Quirk00 <beata@obrok.eu>
2014-10-06 20:07:17 +0200

    Updated note model

--- app/models/note.server.model.js
+++ app/models/note.server.model.js
@@ -10,11 +10,25 @@ var mongoose = require('mongoose'),
  * Note Schema
  */
 var NoteSchema = new Schema({
-  title: String,
+  title: {
+    type: String,
+    default: 'Untitled'
+  },
   contentPlain: String,
   user: {
     type: Schema.Types.ObjectId,
     ref: 'User'
+  },
+  tags: {
+    type: [ String ],
+    index: true
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  modified: {
+    type: Date
   }
 });
 
