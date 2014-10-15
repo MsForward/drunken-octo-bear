be98f4eb5a6056d71661a250e77f082573314870
Quirk00 <beata@obrok.eu>
2014-10-15 22:17:28 +0200

    Configure protractor to use PhantomJS

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,4 +1,9 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['tests/e2e/**/*.test.js']
+  specs: ['tests/e2e/**/*.test.js'],
+  capabilities: {
+    browserName: 'PhantomJS',
+    'phantomjs.binary.path': require('phantomjs').path,
+    'phantomjs.ghostdriver.cli.args': [ '--loglevel=DEBUG' ]
+  }
 }
